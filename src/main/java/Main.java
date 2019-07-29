29d5108ec786034f4cc05afa8761f6f544f828a2
Oleksii Tumanov <a.tumanov@agilites.com>
2019-07-29 14:26:55 +0300

    fix print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,47 +4,47 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) {
-
-        if (Files.exists(new File("G:/_Process/A&P_Invoice_Backup_Files").toPath())) {
-            System.out.println("Exist!");
-        } else {
-            System.out.println("Not Exist!");
-        }
-
-        if (Files.exists(new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files").toPath())) {
-            System.out.println("Exist!");
-        } else {
-            System.out.println("Not Exist!");
-        }
-
-
-        Path res = null;
-        try {
-            res = Files.createDirectories(new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files/test").toPath());
-        } catch (IOException e) {
-            System.out.println("Can't create a folder! ");
-        }
-
-        if (Files.exists(res)) {
-            try {
-                Files.copy(new File("txt.txt").toPath(),new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files/test//txt.ttt").toPath());
-            } catch (IOException e) {
-                System.out.println("File copy error!");
-            }
-            System.out.println("File copied!");
-        } else {
-            System.out.println("folder Not Created!");
-        }
-
-
-
-//        Path f = new File("test").toPath();
+    public static void main(String[] args) throws IOException {
 //
-//        if (!Files.exists(f)){
-//            Path res = Files.createDirectories(f);
+//        if (Files.exists(new File("G:/_Process/A&P_Invoice_Backup_Files").toPath())) {
+//            System.out.println("Exist!");
+//        } else {
+//            System.out.println("Not Exist!");
 //        }
-//        Files.copy(new File("txt.txt").toPath(),new File("test/txt.ttt").toPath());
+//
+//        if (Files.exists(new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files").toPath())) {
+//            System.out.println("Exist!");
+//        } else {
+//            System.out.println("Not Exist!");
+//        }
+//
+//
+//        Path res = null;
+//        try {
+//            res = Files.createDirectories(new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files/test").toPath());
+//        } catch (IOException e) {
+//            System.out.println("Can't create a folder! ");
+//        }
+//
+//        if (Files.exists(res)) {
+//            try {
+//                Files.copy(new File("txt.txt").toPath(),new File("//chfcl044.dufry.net/groups$/CH_2_Mountpoint/Groups/CH/_Process/A&P_Invoice_Backup_Files/test//txt.ttt").toPath());
+//            } catch (IOException e) {
+//                System.out.println("File copy error!");
+//            }
+//            System.out.println("File copied!");
+//        } else {
+//            System.out.println("folder Not Created!");
+//        }
+
+
+
+        Path f = new File("test").toPath();
+
+        if (!Files.exists(f)){
+            Path res = Files.createDirectories(f);
+        }
+        Files.copy(new File("txt.txt").toPath(),new File("test/txt.txt").toPath());
 
     }
 }
